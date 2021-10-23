e672ddd2a635d26eee4d18cb75dc382d2feb75ee
Trevor Tomlin <32802101+trevortomlin@users.noreply.github.com>
2021-10-23 15:15:27 -0700

    Generates banner correctly.

--- tbg.sh
+++ tbg.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 bg="░"
 tlline="┌"
 hline="─"
@@ -8,67 +6,110 @@ trline="┐"
 blline="└"
 brline="┘"
 
-a=("█▀█" "█▀█" "▀░▀")
-b=("█▀▄" "█▀▄" "▀▀░")
-c=("█▀▀" "█░░" "▀▀▀")
-d=("█▀▄" "█░█" "▀▀░")
-e=("█▀▀" "█▀▀" "▀▀▀")
-f=("█▀▀" "█▀▀" "▀░░")
-g=("█▀▀" "█░█" "▀▀▀")
-h=("█░█" "█▀█" "▀░▀")
-i=("▀█▀" "░█░" "▀▀▀")
-j=("▀▀█" "▄░█" "▀▀▀")
-k=("█░█" "██░" "█░█")
-l=("█░░" "█░░" "▀▀▀")
-m=("█▄█" "█░█" "▀░▀")
-n=("█▀█" "█░█" "▀░▀")
-o=("█▀█" "█░█" "▀▀▀")
-p=("█▀█" "█▀▀" "▀░░")
-q=("█▀█" "█▄█" "░░▀")
-r=("█▀▄" "█▀▄" "▀░▀")
-s=("█▀▀" "▀▀█" "▀▀▀") 
-t=("▀█▀" "░█░" "░█░")
-u=("█░█" "█░█" "▀▀▀")
-v=("█░█" "▀▄▀" "░▀░")
-w=("█░█" "█▄█" "▀░▀")
-x=("█░█" "░█░" "█░█")
-y=("█░█" "░█░" "░▀░")
-z=("▀▀█" "▄▀" "▀▀▀")
+declare -A charArray
 
-echo -n $tlline
-for i in {0..11..1}
-do
-  echo -n $hline
-done
+charArray[a]="█▀██▀█▀░▀"
+charArray[b]="█▀▄█▀▄▀▀░"
+charArray[c]="█▀▀█░░▀▀▀"
+charArray[d]="█▀▄█░█▀▀░"
+charArray[e]="█▀▀█▀▀▀▀▀"
+charArray[f]="█▀▀█▀▀▀░░"
+charArray[g]="█▀▀█░█▀▀▀"
+charArray[h]="█░██▀█▀░▀"
+charArray[i]="▀█▀░█░▀▀▀"
+charArray[j]="▀▀█▄░█▀▀▀"
+charArray[k]="█░███░█░█"
+charArray[l]="█░░█░░▀▀▀"
+charArray[m]="█▄██░█▀░▀"
+charArray[n]="█▀██░█▀░▀"
+charArray[o]="█▀██░█▀▀▀"
+charArray[p]="█▀██▀▀▀░░"
+charArray[q]="█▀██▄█░░▀"
+charArray[r]="█▀▄█▀▄▀░▀"
+charArray[s]="█▀▀▀▀█▀▀▀"
+charArray[t]="▀█▀░█░░▀░"
+charArray[u]="█░██░█▀▀▀"
+charArray[v]="█░█▀▄▀░▀░"
+charArray[w]="█░██▄█▀░▀"
+charArray[x]="█░█░█░█░█"
+charArray[y]="█░█░█░░▀░"
+charArray[z]="▀▀█░▄▀▀▀▀"
 
-echo $trline
+charArray[:]="░░░░▀░░▀░"
+charArray[!]="░█░░▀░░▀░"
+charArray[?]="▀▀█░▀░░▀░"
 
-for i in {0..2..1}
-do
+read text
+
+print_chars(){
+
+  for row in {0..2..1}
+  do
+
+    for ((col=0;col<${#1};col+=1)); do
+
+    char=${1:col:1}
+    if [ $col -eq 0 ];
+
+    then
+      echo -n $vline
+
+    fi
+    echo -n ░
+    echo -n ${charArray[$char]:row*3:3}
+    echo -n ░
+
+    if [ $col -eq $((${#1}-1)) ];
+
+    then
+      echo -n $vline
+
+    fi
+
+  done
+
+  echo
+
+  done
+
   echo -n $vline
-  echo -n $bg
-  echo -n ${a[i]}
-  echo -n $bg
-  echo -n ${b[i]}
-  echo -n $bg
-  echo -n ${c[i]}
-  echo $vline
-done
 
-echo -n $blline
-for i in {0..11..1}
-do
-  echo -n $hline
-done
+  for ((it=0;it<${#1}*5;it+=1)); do
 
-echo $brline
+    echo -n $bg
 
-for item in "${a[@]}";
-do
-	echo $item
-done
+  done
 
-for item in "${b[@]}";
-do
-	echo $item
-done
+  echo  $vline
+
+}
+
+print_top(){
+
+  echo -n $tlline
+  for i in $(seq 0 $1);
+  do
+    echo -n $hline
+  done
+  
+  echo $trline
+
+}
+
+print_bottom(){
+
+  echo -n $blline
+  for i in $(seq 0 $1);
+  do
+    echo -n $hline
+  done
+  
+  echo $brline
+
+}
+
+print_top $((${#text}*5-1))
+
+print_chars $text
+
+print_bottom $((${#text}*5-1))
